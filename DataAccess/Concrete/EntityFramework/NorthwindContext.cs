ee8ffd156727218f316807dd253b5e5f70c02bab
arif-odemis <arif.odemis@hotmail.com>
2022-10-29 02:59:18 +0300

    9.ders sonu

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -6,16 +6,18 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-	public class NorthwindContext:DbContext
+	public class NorthwindContext : DbContext
 	{
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
 
 		}
-		public DbSet<Product>Products { get; set; }
+		public DbSet<Product> Products { get; set; }
 		public DbSet<Category> Categories { get; set; }
 		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Order> Orders { get; set; }
+
 
 	}
 }
