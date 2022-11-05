23a502a9c2e4d144efa86a1d22f3859d5158ffdf
arif-odemis <arif.odemis@hotmail.com>
2022-11-05 14:51:45 +0300

    11.ders

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ProductsController : ControllerBase
+	{
+		IProductService _productService;
+
+		public ProductsController(IProductService productService)
+		{
+			_productService = productService;
+		}
+
+		[HttpGet("getall")]
+		public IActionResult GetAll()
+		{
+
+			var result = _productService.GetAll();
+			if (result.Success)
+			{
+				return Ok(result);
+			}
+			return BadRequest(result);
+		}
+		[HttpGet("getbyid")]
+		public IActionResult Get(int id)
+		{
+
+			var result = _productService.GetById(id);
+			if (result.Success)
+			{
+				return Ok(result);
+			}
+			return BadRequest(result);
+		}
+
+		[HttpPost("add")]
+		public IActionResult Post(Product product)
+		{
+			var result = _productService.Add(product);
+			if (result.Success)
+			{
+				return Ok(result);
+			}
+			return BadRequest(result);
+		}
+
+
+
+	}
+}
