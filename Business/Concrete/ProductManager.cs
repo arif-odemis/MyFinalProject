07b12ce370ad2e5f37426e495c861ad1a0adf29c
arif-odemis <arif.odemis@hotmail.com>
2022-11-07 10:59:16 +0300

    12.ders sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,10 +1,14 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,19 +17,17 @@ namespace Business.Concrete
 {
 	public class ProductManager : IProductService
 	{
+	
 		IProductDal _productDal;
 
 		public ProductManager(IProductDal productDal)
 		{
 			_productDal = productDal;
 		}
-
+		[ValidationAspect(typeof(ProductValidator))]
 		public IResult Add(Product product)
 		{
-			if (product.ProductName.Length < 2)
-			{
-				return new ErorResult(Messages.ProductNameInvalid);
-			}
+		
 			_productDal.Add(product);
 			return new SuccessResult(Messages.ProductAdded);
 
